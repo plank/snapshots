3fbcdba4f9e27a9f93a3d6df0c242ecddc5f8929
Kurt Friars <kurt@plankdesign.com>
2023-09-06 14:54:42 -0400

    Add dd to db driver for table copy factory

--- src/Factories/TableCopierFactory.php
+++ src/Factories/TableCopierFactory.php
@@ -12,7 +12,7 @@ class TableCopierFactory
 {
     public static function forDriver(string $driver): CopiesTables
     {
-        dump($driver);
+        dd($driver);
         switch ($driver) {
             case 'mysql':
                 return new MySqlTableCopier();
