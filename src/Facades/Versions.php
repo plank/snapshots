d0ae2d1c5bc53761593a973153872c8958e7bcab
Massimo Triassi <massimo@plankdesign.com>
2024-03-22 11:40:44 -0400

    docs: #15 add method annotations to get intellisense function listings

--- src/Facades/Versions.php
+++ src/Facades/Versions.php
@@ -2,9 +2,20 @@
 
 namespace Plank\Snapshots\Facades;
 
+use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\Facade;
 use Plank\Snapshots\Contracts\ManagesVersions;
+use Plank\Snapshots\Contracts\Version;
 
+/**
+ * @method void setActive(?Version $version)
+ * @method void clearActive()
+ * @method Version|null active()
+ * @method Version|null latest()
+ * @method Version|null find($key)
+ * @method Version|null byNumber(string $number)
+ * @method Collection all()
+ */
 class Versions extends Facade
 {
     /**
