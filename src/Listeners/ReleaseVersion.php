00f4639744b906e05c89d49eeecd18822c9a8a8e
Kurt Friars <kfriars@apple.com>
2025-03-03 14:37:39 -0500

    Make snapshots responsible for handling the schema events so we can batch them.

---
+++ src/Listeners/ReleaseVersion.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Plank\Snapshots\Listeners;
+
+use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Event;
+use Plank\LaravelSchemaEvents\Events\TableCreated;
+use Plank\LaravelSchemaEvents\Facades\SchemaEvents;
+use Plank\Snapshots\Events\VersionCreated;
+use Plank\Snapshots\Events\VersionMigrated;
+use Plank\Snapshots\Exceptions\MigrationFailedException;
+
+class ReleaseVersion
+{
+    public function handle(VersionCreated $event)
+    {
+        // Flush any previously recorded events
+        SchemaEvents::get();
+
+        if (Artisan::call('migrate') !== 0) {
+            throw MigrationFailedException::create($event->version);
+        }
+
+        $tables = SchemaEvents::get()
+            ->filter(fn ($schemaEvent) => $schemaEvent instanceof TableCreated)
+            ->map(function (TableCreated $created) {
+                return $created->table;
+            })
+            ->all();
+
+        $version = $event->version;
+        $version->migrated = true;
+        $version->save();
+
+        Event::dispatch(new VersionMigrated($event->version, $tables));
+    }
+}
