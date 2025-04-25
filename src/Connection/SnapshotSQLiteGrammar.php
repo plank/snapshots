51f2d86d8aff65cea175b306f7eb18ac8bd8d11b
Kurt Friars <kfriars@apple.com>
2025-04-25 19:46:29 -0400

    Extract unversioned foreign key commands to the grammar layer

--- src/Connection/SnapshotSQLiteGrammar.php
+++ src/Connection/SnapshotSQLiteGrammar.php
@@ -29,6 +29,18 @@ class SnapshotSQLiteGrammar extends SQLiteGrammar
     }
 
     /**
+     * Compile a foreign key command.
+     *
+     * @param  \Illuminate\Database\Schema\Blueprint  $blueprint
+     * @param  \Illuminate\Support\Fluent  $command
+     * @return string
+     */
+    public function compileUnversionedForeign(Blueprint $blueprint, Fluent $command)
+    {
+        // Handled on table creation...
+    }
+
+    /**
      * Get the foreign key syntax for a table creation statement.
      *
      * @param  \Illuminate\Database\Schema\ForeignKeyDefinition[]  $foreignKeys
@@ -37,15 +49,44 @@ class SnapshotSQLiteGrammar extends SQLiteGrammar
     protected function addUnversionedForeignKeys($foreignKeys)
     {
         return (new Collection($foreignKeys))->reduce(function ($sql, ForeignKeyDefinition $foreign) {
-            /** @var class-string<VersionKey> $keyClass */
-            $keyClass = config('snapshots.value_objects.version_key');
-
-            $foreign->on($keyClass::strip($foreign->on));
-
             // Once we have all the foreign key commands for the table creation statement
             // we'll loop through each of them and add them to the create table SQL we
             // are building, since SQLite needs foreign keys on the tables creation.
-            return $sql.$this->getForeignKey($foreign);
+            return $sql.$this->getUnversionedForeignKey($foreign);
         }, '');
+    }
+
+    /**
+     * Get the SQL for the foreign key.
+     *
+     * @param  \Illuminate\Support\Fluent  $foreign
+     * @return string
+     */
+    protected function getUnversionedForeignKey($foreign)
+    {
+        /** @var class-string<VersionKey> $keyClass */
+        $keyClass = config('snapshots.value_objects.version_key');
+
+        // We need to columnize the columns that the foreign key is being defined for
+        // so that it is a properly formatted list. Once we have done this, we can
+        // return the foreign key SQL declaration to the calling method for use.
+        $sql = sprintf(', foreign key(%s) references %s(%s)',
+            $this->columnize($foreign->columns),
+            $keyClass::strip($this->wrapTable($foreign->on)),
+            $this->columnize((array) $foreign->references)
+        );
+
+        if (! is_null($foreign->onDelete)) {
+            $sql .= " on delete {$foreign->onDelete}";
+        }
+
+        // If this foreign key specifies the action to be taken on update we will add
+        // that to the statement here. We'll append it to this SQL and then return
+        // this SQL so we can keep adding any other foreign constraints to this.
+        if (! is_null($foreign->onUpdate)) {
+            $sql .= " on update {$foreign->onUpdate}";
+        }
+
+        return $sql;
     }
 }
