129f68f1c9d4e6e462e71dfecff08101a0345928
Kurt Friars <kfriars@apple.com>
2025-05-22 12:34:42 +0000

    Add config flag to force versioned content to only migrate for existing versions

---
+++ tests/Suites/Feature/Migrations/ForceVersionsTest.php
@@ -0,0 +1,50 @@
+<?php
+
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Event;
+use Plank\Snapshots\Contracts\ManagesVersions;
+use Plank\Snapshots\Contracts\Version;
+use Plank\Snapshots\Events\DataCopied;
+use Plank\Snapshots\Repository\VersionRepository;
+use Plank\Snapshots\Tests\Models\Document;
+
+use function Pest\Laravel\artisan;
+use function Pest\Laravel\assertDatabaseHas;
+use function Pest\Laravel\assertDatabaseMissing;
+
+describe('The CopyTables listener correctly copies data', function () {
+    beforeEach(function () {
+        config()->set('snapshots.force_versions', true);
+
+        app()->instance(ManagesVersions::class, new class extends VersionRepository {
+            public function working(?Version $version): ?Version
+            {
+                if ($latest = $this->latest()) {
+                    return $latest->getKey() !== $version?->getKey()
+                        ? $latest
+                        : null;
+                }
+
+                return null;
+            }
+        });
+
+        artisan('migrate', [
+            '--path' => migrationPath('schema/create'),
+            '--realpath' => true,
+        ])->run();
+    });
+
+    it('does not create versioned tables until a version is created', function () {
+        assertDatabaseMissing('migrations', [
+            'migration' => 'create_documents_table',
+        ]);
+
+        createFirstVersion('schema/create');
+
+        assertDatabaseHas('migrations', [
+            'migration' => 'v1_0_0_create_documents_table',
+            'batch' => 3,
+        ]);
+    });
+});
